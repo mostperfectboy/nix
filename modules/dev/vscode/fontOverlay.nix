78ac1e50813226c09b64c340e230eeaedf3024e5
Niko <git@undefine.dev>
2024-10-02 22:13:35 +0200

    remove unused rec

--- modules/dev/vscode/fontOverlay.nix
+++ modules/dev/vscode/fontOverlay.nix
@@ -1,5 +1,5 @@
 self: super: {
-  vscode = super.vscode.overrideAttrs (oldAttrs: rec {
+  vscode = super.vscode.overrideAttrs (oldAttrs: {
     postPatch = oldAttrs.postPatch + ''
       echo '.mtki { font-family: Operator Mono Lig, Operator Mono; } ' >> ./resources/app/out/vs/workbench/workbench.desktop.main.css
     '';
