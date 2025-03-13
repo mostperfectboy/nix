e0894fb340b089d3c7301a6f34aeb583ad415e72
Niko <git@undefine.dev>
2025-03-13 13:44:38 +0100

    add configuration of vscode password store

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 #### Niko NixOS
-
-vscode keyrings:
-
-- add `password-store: "gnome"` to `~/.vscode/argv.json`
--- homes/niko/dev/vscode.nix
+++ homes/niko/dev/vscode.nix
@@ -4,4 +4,11 @@
   programs.vscode = {
     enable = true;
   };
+  home.file.".vscode/argv.json" = {
+    text = builtins.toJSON {
+    enable-crash-reporter = false;
+    password-store = "gnome";
+  };
+    force = true;
+  };
 }
