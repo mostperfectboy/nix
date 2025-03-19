4b9724224ebdff5586ab0028847f8ff0ae3d8281
Niko <git@undefine.dev>
2025-03-19 16:38:23 +0100

    add sensible git defaults

--- homes/niko/shell.nix
+++ homes/niko/shell.nix
@@ -16,5 +16,27 @@
     enable = true;
     userName = "Niko";
     userEmail = "git@undefine.dev";
+    extraConfig = {
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "main";
+      diff = {
+        algorithm = "histogram";
+        mnemonicPrefix = true;
+        rename = true;
+      };
+      push = {
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = true;
+      pull.rebase = true;
+    };
   };
 }
