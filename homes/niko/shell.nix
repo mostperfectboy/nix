ca01cca91f5f9158eaa3b3f9cb27a3bf3c080efd
Niko <git@undefine.dev>
2025-05-13 15:25:57 +0200

    move packages into hm options

--- homes/niko/shell.nix
+++ homes/niko/shell.nix
@@ -4,39 +4,48 @@
 
   home.packages = with pkgs; [
     wget
-    git
     nixd
-    nh
-    gitui
-    zellij
     nixfmt-rfc-style
   ];
 
-  programs.git = {
-    enable = true;
-    userName = "Niko";
-    userEmail = "git@undefine.dev";
-    extraConfig = {
-      column.ui = "auto";
-      branch.sort = "-committerdate";
-      tag.sort = "version:refname";
-      init.defaultBranch = "main";
-      diff = {
-        algorithm = "histogram";
-        mnemonicPrefix = true;
-        rename = true;
+  programs = {
+    git = {
+      enable = true;
+      userName = "Niko";
+      userEmail = "git@undefine.dev";
+      extraConfig = {
+        column.ui = "auto";
+        branch.sort = "-committerdate";
+        tag.sort = "version:refname";
+        init.defaultBranch = "main";
+        diff = {
+          algorithm = "histogram";
+          mnemonicPrefix = true;
+          rename = true;
+        };
+        push = {
+          autoSetupRemote = true;
+          followTags = true;
+        };
+        fetch = {
+          prune = true;
+          pruneTags = true;
+          all = true;
+        };
+        help.autocorrect = true;
+        pull.rebase = true;
       };
-      push = {
-        autoSetupRemote = true;
-        followTags = true;
-      };
-      fetch = {
-        prune = true;
-        pruneTags = true;
-        all = true;
-      };
-      help.autocorrect = true;
-      pull.rebase = true;
+    };
+    gitui = {
+      enable = true;
+    };
+    nh = {
+      enable = true;
+      clean.enable = true;
+    };
+    zellij = {
+      enable = true;
+      enableFishIntegration = true;
     };
   };
 }
