c676552ad15467b49c2769e7b619dc282b5238ed
Niko <git@undefine.dev>
2025-11-16 02:19:38 +0100

    remove vscode font overlay
    since using victor mono now there's no need to patch in an italics font

--- homes/niko/dev/vscode.nix
+++ homes/niko/dev/vscode.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  nixpkgs.overlays = [ (import ./vscode/fontOverlay.nix) ];
   home.packages = with pkgs; [
     vscode
     zed-editor
--- homes/niko/dev/vscode/fontOverlay.nix
+++
@@ -1,9 +0,0 @@
-self: super: {
-  vscode = super.vscode.overrideAttrs (oldAttrs: {
-    postPatch =
-      oldAttrs.postPatch
-      + ''
-        echo '.mtki { font-family: VictorMono Nerd Font Mono, Operator Mono Lig, Operator Mono; font-weight: 700 !important } ' >> ./resources/app/out/vs/workbench/workbench.desktop.main.css
-      '';
-  });
-}
