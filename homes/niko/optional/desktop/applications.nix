755e554a03b855a4c8188f6529b76fb8d2744718
Niko <git@undefine.dev>
2026-02-03 23:31:31 +0100

    remove obs

--- homes/niko/optional/desktop/applications.nix
+++ homes/niko/optional/desktop/applications.nix
@@ -1,13 +1,5 @@
 { pkgs, ... }:
 {
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs; [
-      obs-studio-plugins.obs-move-transition
-      obs-studio-plugins.obs-tuna
-      obs-studio-plugins.input-overlay
-    ];
-  };
   home.packages = with pkgs; [
     spotify
     nautilus
